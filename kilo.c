8decea27f49cde01230ee153cedf57529d7425b1
Chris Tcherkezian <ctcherkezian@cpp.edu>
2021-12-05 15:07:50 -0800

    fixed typos

--- kilo.c
+++ kilo.c
@@ -89,7 +89,7 @@ struct editorConfig {
     time_t statusmsg_time;
     struct editorSyntax *syntax;
     struct termios orig_termios;
-}
+};
 
 struct editorConfig E;
 
@@ -206,7 +206,7 @@ int getCursorPosition(int *rows, int *cols) {
     if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
 
     while (i < sizeof(buf) - 1) {
-        if (red(STDIN_FILENO, &buf[i], 1) != 1) break;
+        if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
         if (buf[i] == 'R') break;
         i++
     }
@@ -355,9 +355,8 @@ void editorUpdateSyntax(erow *row) {
 
 int editorSyntaxToColor(int hl) {
   switch (hl) {
-    case HL_COMMENT:
-    case HL_MLCOMMENT: return 36;
-    case HL_COMMENT: return 36;
+    case HL_COMMENT: 
+    case HL_MLCOMMENT: return 36; 
     case HL_KEYWORD1: return 33;
     case HL_KEYWORD2: return 32;
     case HL_STRING: return 35;
@@ -802,30 +801,30 @@ void editorDrawRows(struct abuf *ab) {
         }
 }
 
+
 void editorDrawStatusBar(struct abuf *ab) {
-    abAppend(ab, "\x1b[7m", 4);
-    char status[80], rstatus[80];
-    int len = snprintf(status, sizeof(status), "%.20s - %d lines %s",
-        E.filename ? E.filename: "[No Name]", E.numrows),
-        E.dirty ? "(modifies)" : "");
-         int rlen = snprintf(rstatus, sizeof(rstatus), "%s | %d/%d",
+  abAppend(ab, "\x1b[7m", 4);
+  char status[80], rstatus[80];
+  int len = snprintf(status, sizeof(status), "%.20s - %d lines %s",
+    E.filename ? E.filename : "[No Name]", E.numrows,
+    E.dirty ? "(modified)" : "");
+  int rlen = snprintf(rstatus, sizeof(rstatus), "%s | %d/%d",
     E.syntax ? E.syntax->filetype : "no ft", E.cy + 1, E.numrows);
-    int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d", 
-        E.cy + 1, E.numrows);
-    if(len > E.screencols) len = E.screencols;
-    abAppend(ab, status, len);
-    while (len < E.screencols) {
-        if(E.screencols - len == rlen) {
-            abAppend(ab, rstatus, rlen);
-            break;
-            } else {
-        abAppend(ab, " ", 1);
-        len++;
-            }
+  if (len > E.screencols) len = E.screencols;
+  abAppend(ab, status, len);
+  while (len < E.screencols) {
+    if (E.screencols - len == rlen) {
+      abAppend(ab, rstatus, rlen);
+      break;
+    } else {
+      abAppend(ab, " ", 1);
+      len++;
     }
-    abAppend(ab, "\x1b[m", 3);
-    abAppend(ab, "\r\n", 2);
+  }
+  abAppend(ab, "\x1b[m", 3);
+  abAppend(ab, "\r\n", 2);
 }
+
 
 void editorDrawMessageBar(struct abuf *ab){
     abAppend(ab, "\x1b[K", 3);
@@ -920,7 +919,7 @@ void editorMoveCursor(int key) {
         case ARROW_RIGHT:
         if(row && E.cx < row->size){
             E.cx++;
-        } else if (row & E.cx == row->size){
+        } else if (row && E.cx == row->size){
             E.cy++;
             E.cx = 0;
         }
@@ -1014,7 +1013,7 @@ void editorProcessKeypress() {
             break;
 
         case CTRL_KEY('l'):
-        case: '\x1b':
+        case '\x1b':
         break;
 
         default:
